a424d3904c1940c87c67ada6b4d512e243d00a79
RockyPraxe <filipio.svanda@gmail.com>
2023-11-30 01:05:11 +0000

    Product Admin - add product view, url and template

--- products/models.py
+++ products/models.py
@@ -5,15 +5,15 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def gert_friendly_name(self):
-        return serlf.friendly_name
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
